6964325df29d744f3acafb5081fd9ea6d8c4e018
olya <olyaboricova@gmail.com>
2021-01-02 20:30:30 +0300

    now we can run tests

--- main.py
+++ main.py
@@ -6,36 +6,41 @@ import sys
 import reverse_polish_notation as rev_pn
 import cnf
 
+def main(filename):
+    if filename == None:
+        arg_pr = argparse.ArgumentParser(description=' ', add_help=True, conflict_handler='resolve')
+        arg_pr.add_argument('-f', '--file', action='store', type = str, dest='file_name', help='to use file name format')
+        arg_pr.add_argument('-n', '--input', action='store_true', dest='input', help='to use input format and stop it by cntr+D')
 
-arg_pr = argparse.ArgumentParser(description=' ', add_help=True, conflict_handler='resolve')
-arg_pr.add_argument('-f', '--file', action='store', type = str, dest='file_name', help='file name')
-arg_pr.add_argument('-n', '--input', action='store_true', dest='input', help='to use input format')
-
-args = arg_pr.parse_args(sys.argv[1::])
-
-if args.file_name == None and args.input != True:
-    #print args help
-    print("add file or choose input format")
-    exit()
-if (args.file_name != None):
+        args = arg_pr.parse_args(sys.argv[1::])
+        if args.file_name == None and args.input != True:
+            arg_pr.print_help()
+        elif args.file_name != None:
+            filename = args.file_name
+        else:
+            filename = 0
+    
     try:
-        fd = open(args.file_name)
+        fd = open(filename)
     except IOError as e:
-        exit(e)
-else:
-    fd = sys.stdin
+            exit(e)
 
-data = pars.get_data(fd)
-fd.close()
-print (data)
-rpn_rules = []
-for rule in data["rules"]:
-    rpn_rule = rev_pn.rpn(rule)
-    print(rpn_rule)
-    rpn_rules.append(rpn_rule)
-data["rpn_rules"] = rpn_rules
-print(data)
-# full_cnf, events_list = 
-cnf.analyze_problem(data)
-# result = cnf.check_queries(data, full_cnf, events_list)
-# print(result)
+    data = pars.get_data(fd)
+    fd.close()
+    rpn_rules = []
+
+    for rule in data["rules"]:
+        rpn_rule = rev_pn.rpn(rule)
+        #print(rpn_rule)
+        rpn_rules.append(rpn_rule)
+    data["rpn_rules"] = rpn_rules
+    #print(data)
+    # full_cnf, events_list = 
+    #print("START TASK")
+    result = cnf.analyze_problem(data)
+    # result = cnf.check_queries(data, full_cnf, events_list)
+    # print(result)
+    return result
+
+if __name__ == '__main__':
+    main(None)
--- samples/test.py
+++ samples/test.py
@@ -1,27 +1,32 @@
 import os
 import json
 from glob import glob
-from parse_file import parse_file
-from global_graph import Fact
-from expert_system import expert_system
+#from parse_file import parse_file
+#from global_graph import Fact
+#from expert_system import expert_system
+import sys
+sys.path.insert(0,'/home/olya/mygit_expert_system')
 
+from main import main
 
 DO_PRINT = True
-PATH = './samples/check_list/'
+#PATH = './samples/check_list/'
+PATH = './samples/peers_check/'
 with open('./samples/answers.json', 'r') as f:
     dict_answers = json.load(f)
 
 for filename in glob(PATH + "**/**", recursive=True):
     if os.path.isfile(filename):
         try:
-            list_rules, init_facts, query = parse_file(filename)
-            result = expert_system(list_rules, init_facts, query)
+            #list_rules, init_facts, query = parse_file(filename)
+            #result = expert_system(list_rules, init_facts, query)
+            result = main(filename)
         except SystemExit:
             result = {}
         answer = dict_answers.get(filename, {})
         check = answer == result
-        Fact.dict_facts = {}
-        Fact.check_contradiction = False
+        #Fact.dict_facts = {}
+        #Fact.check_contradiction = False
         print(filename, check)
         if DO_PRINT:
             print("result process: ", result)
